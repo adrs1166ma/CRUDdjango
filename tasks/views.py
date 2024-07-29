6523f547cab0c989cb458b316b7a700c46864efe
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-07-29 09:36:05 -0500

    handling errors

--- tasks/views.py
+++ tasks/views.py
@@ -5,30 +5,36 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home.html')
 
+
 def signup(request):
-    
+
     if request.method == 'GET':
-        print ('entrando al formulario')
+        print('entrando al formulario')
         return render(request, 'signup.html', {
-        'form': UserCreationForm
+            'form': UserCreationForm,
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                #register user
+                # register user
                 user = User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1']
+                    username=request.POST['username'],
+                    password=request.POST['password1']
                 )
                 user.save()
                 return HttpResponse('User created successfully')
             except:
-                return HttpResponse('Username already exists')
-        print(request.POST)
-        print('obteniendo datos')
-        return HttpResponse('Password do not match')
-        
-    
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'User name alredy exists',
+                })
+        # print(request.POST)
+        # print('obteniendo datos')
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match',
+        })
