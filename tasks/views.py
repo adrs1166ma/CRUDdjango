fad951fd14531991e1872cc09602efb89337b0d4
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-07-29 20:58:48 -0500

    get task

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -68,7 +68,11 @@ def create_task(request):
                 'error': 'Please provide valida data'
             })
             
-    
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'task_detail.html', {
+        'task': task
+    })
     
 
 def signout(request):
